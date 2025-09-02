0bba133c0737693e3cbc93595f66a0f96b37e12b
IvanTudtud-UCM <ivantudtud1@gmail.com>
2025-09-02 21:38:01 +0800

    Create TableAndChairs.java

---
+++ Chapter_1/TableAndChairs.java
@@ -0,0 +1,17 @@
+public class TableAndChairs {
+    public static void main (String[] args) {
+        int i;
+        for (i = 1; i <= 5; i++){
+            System.out.println("x                                               x");
+        }
+        for (i = 5; i == 5; i++){
+            System.out.println("x         xxxxxxxxxxxxxxxxxxxxxxxxxxxxx         x");
+        }
+        for (i = 6; i == 6; i++){
+            System.out.println("x x x x x x     x               x     x x x x x x");
+        }
+        for (i = 7; i <= 10; i++){
+            System.out.println("x         x     x               x     x         x");
+        }
+    }
+}
