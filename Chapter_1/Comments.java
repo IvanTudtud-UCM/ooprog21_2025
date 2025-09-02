d9d3970aa2d8cf67228b39dfac9f93a10de139f5
IvanTudtud-UCM <ivantudtud1@gmail.com>
2025-09-02 21:51:13 +0800

    Create Comments.java

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,21 @@
+public class Comments {
+    public static void main(String[] args) {
+        
+        // Single-line Comment: This prints a welcome message
+        System.out.println("Hello World");
+        
+        /* 
+           Multi-line Comment: 
+           The next line prints another message. 
+           You can use this style for longer explanations.
+        */
+        System.out.println("Hello World");
+        
+        /**
+         * Documentation Comment:
+         * This line shows how doc comments look in code.
+         * Normally used for classes and methods.
+         */
+        System.out.println("Hello World");
+    }
+}
