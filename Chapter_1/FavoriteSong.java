a1d69d17e92585e1b278ebac8c0b1bc91acebe54
IvanTudtud-UCM <ivantudtud1@gmail.com>
2025-09-02 22:28:59 +0800

    Create FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,11 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        
+        System.out.println("If you leave me, don't you ever think I wan't you to go");
+        System.out.println("Cause I'm bound to you");
+        System.out.println("All I need is you to wait for me, and I'll be there for you");
+        System.out.println("Cause I'll run to you");
+        System.out.println("My heart belongs to you");
+        System.out.println("I'll take a piece of you");
+    }
+}
