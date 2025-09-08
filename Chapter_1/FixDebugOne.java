28aead32a6b71bd8767a183adc5cefa4fa24c109
IvanTudtud-UCM <ivantudtud1@gmail.com>
2025-09-08 19:00:59 +0800

    Create FixDebugOne.java

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,19 @@
+public class FixDebug One
+{
+/* This program displays a greeting */
+public static void Main(String args)
+{
+Systm.out.println("Hello World!")
+}
+/*
+List down the spotted bugs below:
+e.g.
+- // wrong syntax for access modifier "publlic" instead of "public"
+Bugs:
+1. wrong class syntax "FixDebug One" should be "FixDebugOne"
+2. lacking one closing bracket for the class FixDebugOne
+3. wrong method name "Main" should be "main"
+4. lacking square bracket after the String
+5. Typo in "Systm" it should be "System"
+6. Missing semicolon after println statement
+*/
