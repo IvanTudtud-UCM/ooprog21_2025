63751040f496568c5eb57741ce5ba696c3dbff6f
IvanTudtud-UCM <ivantudtud1@gmail.com>
2025-09-15 08:54:44 +0800

    Create CompanyInfo.java

---
+++ Chapter_3/CompanyInfo.java
@@ -0,0 +1,15 @@
+public class CompanyInfo {
+
+   public static void main(String[] args) {
+
+      System.out.println("Smart Electronics");
+      
+      displayHours();
+   }
+
+   static void displayHours() {
+      System.out.println("Monday - Friday    8 am to 6 pm\nSaturday           8 am to noon\nSunday             Closed");    
+   }
+}
+
+
