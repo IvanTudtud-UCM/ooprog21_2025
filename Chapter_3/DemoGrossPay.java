d4e0d2badf0ec4f904fffffec043adbd37c3fd56
IvanTudtud-UCM <ivantudtud1@gmail.com>
2025-09-17 08:38:16 +0800

    Update DemoGrossPay.java

--- Chapter_3/DemoGrossPay.java
+++ Chapter_3/DemoGrossPay.java
@@ -1,29 +1,29 @@
-import javax.swing.JOptionPane;
-
-   public class DemoGrossPay {
+   import javax.swing.*;
    
-      public static void main (String[] args){
+      public class DemoGrossPay {
       
-         String input1 = JOptionPane.showInputDialog("Enter Worked Hours: ");
-         double inputHours1 = Double.parseDouble(input1);
+         public static void main (String[] args){
          
-         String input2 = JOptionPane.showInputDialog("Enter Worked Hours: ");
-         double inputHours2 = Double.parseDouble(input2);
+            String input1 = JOptionPane.showInputDialog("Enter Worked Hours: ");
+            double inputHours1 = Double.parseDouble(input1);
+            
+            String input2 = JOptionPane.showInputDialog("Enter Worked Hours: ");
+            double inputHours2 = Double.parseDouble(input2);
+            
+            String input3 = JOptionPane.showInputDialog("Enter Worked Hours: ");
+            double inputHours3 = Double.parseDouble(input3);
+                  
+            calculateGross(inputHours1);
+            calculateGross(inputHours2);
+            calculateGross(inputHours3);
+         }
          
-         String input3 = JOptionPane.showInputDialog("Enter Worked Hours: ");
-         double inputHours3 = Double.parseDouble(input3);
-               
-         calculateGross(inputHours1);
-         calculateGross(inputHours2);
-         calculateGross(inputHours3);
+         static void calculateGross(double hour){
+       
+            double hourlyRate = 22.75;
+            double grossPay = hour * hourlyRate;
+            
+            System.out.printf("%s hours at $%s per hour is $%s\n", hour, hourlyRate, grossPay);
+            
+         }
       }
-      
-      static void calculateGross(double hour){
-    
-         double hourlyRate = 22.75;
-         double grossPay = hour * hourlyRate;
-         
-         System.out.printf("%s hours at $%s per hour is $%s\n", hour, hourlyRate, grossPay);
-         
-      }
-   }
