7814efaab2d5bcb18cfd3e8f6266c40f68c81c4b
IvanTudtud-UCM <ivantudtud1@gmail.com>
2025-09-24 08:31:20 +0800

    Create DebugBox.java

---
+++ Chapter_4/DebugBox.java
@@ -0,0 +1,25 @@
+public class DebugBox {
+    private int width;
+    private int length;
+    private int height;
+    
+    public DebugBox() {
+        length = 1;
+        width = 1;
+        height = 1;
+    }
+
+    public DebugBox(int width, int length, int height) {
+        this.width = width;
+        this.length = length;  // Assigned to length
+        this.height = height;  // Assigned to height
+    }
+
+    public void showData() {
+        System.out.println("Width: " + width + " Length: " + length + " Height: " + height);
+    }
+
+    public double getVolume() {
+        return length * width * height;
+    }
+}
