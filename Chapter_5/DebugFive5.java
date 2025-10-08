661bee8a0014459f3ba264957987b19ae815f5c8
IvanTudtud-UCM <ivantudtud1@gmail.com>
2025-10-08 10:51:46 +0800

    Create DebugFive5.java

---
+++ Chapter_5/DebugFive5.java
@@ -0,0 +1,30 @@
+public class Employee {
+    private int employeeNumber;
+    private double payRate;
+    private static final int MAX_EMPLOYEE_NUMBER = 9999;
+    private static final double MAX_RATE = 60.00;
+    private static final double OVERTIME_MULTIPLIER = 1.5;
+
+    public Employee(int employeeNumber, double payRate) {
+        
+        if (employeeNumber > MAX_EMPLOYEE_NUMBER)
+            this.employeeNumber = MAX_EMPLOYEE_NUMBER;
+        else
+            this.employeeNumber = employeeNumber;
+
+        if (payRate > MAX_RATE)
+            this.payRate = MAX_RATE;
+        else
+            this.payRate = payRate;
+    }
+
+    public double calculateRegularPay(double hoursWorked) {
+        double regularHours = Math.min(40, hoursWorked);
+        return regularHours * payRate;
+    }
+
+    public double calculateOvertimePay(double hoursWorked) {
+        double overtimeHours = Math.max(0, hoursWorked - 40);
+        return overtimeHours * payRate * OVERTIME_MULTIPLIER;
+    }
+}
